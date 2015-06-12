1e56e010efa479250166995f1cba8f4406d6c9af
kumarramjee <krram1991@gmail.com>
2015-06-12 19:07:18 +0530

    Git Repository...last

--- app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/ui/MainActivity.java
+++ app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/ui/MainActivity.java
@@ -5,18 +5,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.R;
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.adapter.CityAdapter;
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.beans.CityResult;
+import teleportscreenlatest.mobimedia.com.wheatherapplicastion.parser.ParserCity;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -29,7 +36,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Context activity;
     ArrayList<CityResult> cityResultList;
     CityAdapter adpt;
-
+    String msendcityname;
+    int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +48,53 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         submit.setOnClickListener(this);
 
+        city.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                msendcityname = city.getText().toString().trim();
+                new Thread(new Runnable() {
+                    public void run() {
+                        ParserCity parscity = new ParserCity();
+                        parscity.setText(msendcityname);
+
+                        try {
+                            cityResultList = parscity.getCityList();
+
+                            Log.i("cityActivity", "==" + cityResultList);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (XmlPullParserException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                adpt = new CityAdapter(getApplicationContext(), cityResultList);
+                city.setAdapter(adpt);
+                adpt.notifyDataSetChanged();
+
+            }
+        });
+        city.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                x = position;
+
+
+            }
+        });
     }
 
 
