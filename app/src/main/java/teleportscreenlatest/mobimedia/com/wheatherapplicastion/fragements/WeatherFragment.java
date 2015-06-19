535fdd6a266431b1eea91db8918a401925b99635
kumarramjee <krram1991@gmail.com>
2015-06-19 19:44:15 +0530

    List view.

--- app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/fragements/WeatherFragment.java
+++ app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/fragements/WeatherFragment.java
@@ -1,19 +1,30 @@
 package teleportscreenlatest.mobimedia.com.wheatherapplicastion.fragements;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.R;
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.adapter.WeatherForecastListAdapter;
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.model.WeatherForecast;
+import teleportscreenlatest.mobimedia.com.wheatherapplicastion.ui.MainActivity;
+import teleportscreenlatest.mobimedia.com.wheatherapplicastion.util.FetchForcastForDayJson;
+import teleportscreenlatest.mobimedia.com.wheatherapplicastion.util.FetchJson;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +35,9 @@ public class WeatherFragment extends Fragment {
     TextView mLocationNameTextView;
     TextView mCurrentTemperatureTextView;
     TextView mAttributionTextView;
+    private String mCityname;
+    Context mContext;
+    private Handler mhandler;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -34,6 +48,8 @@ public class WeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+        mhandler=new Handler();
+
         mLocationNameTextView = (TextView) rootView.findViewById(R.id.location_name);
         mCurrentTemperatureTextView = (TextView) rootView
                 .findViewById(R.id.current_temperature);
@@ -46,7 +62,58 @@ public class WeatherFragment extends Fragment {
 
         mAttributionTextView = (TextView) rootView.findViewById(R.id.attribution);
         mAttributionTextView.setVisibility(View.INVISIBLE);
+        GetDetailWeatherDetail(getActivity(), mCityname);
+
+
         return rootView;
+    }
+
+    private void GetDetailWeatherDetail(Activity activity, final String mCityname) {
+
+        if (mCityname.length() == 0) {
+            Toast.makeText(getActivity(), "Not able to find current location.Check ur connection", Toast.LENGTH_SHORT).show();
+
+        } else {
+            new Thread() {
+                public void run() {
+                    final JSONObject json = FetchForcastForDayJson.getJSON(mContext, mCityname);
+                    if (json == null) {
+                        mhandler.post(new Runnable() {
+                            public void run() {
+                                Toast.makeText(mContext,
+                                        "Try after some timee.",
+                                        Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+                    } else {
+                        mhandler.post(new Runnable() {
+                            public void run() {
+                                RenderWeatherDay(json);
+                            }
+                        });
+                    }
+                }
+            }.start();
+        }
+
+
+    }
+
+    private void RenderWeatherDay(JSONObject json) {
+
+
+        try {
+            JSONObject jsondaycode=json.getJSONObject("cod");
+            String city_code=jsondaycode.getString("cod");
+            Log.i("City Code is","City Code"+city_code);
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/ui/CityDetail.java
+++ app/src/main/java/teleportscreenlatest/mobimedia/com/wheatherapplicastion/ui/CityDetail.java
@@ -1,12 +1,20 @@
 package teleportscreenlatest.mobimedia.com.wheatherapplicastion.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.R;
 import teleportscreenlatest.mobimedia.com.wheatherapplicastion.fragements.WeatherFragment;
+import teleportscreenlatest.mobimedia.com.wheatherapplicastion.util.FetchForcastForDayJson;
 
 import static java.lang.Integer.parseInt;
 
@@ -18,14 +26,62 @@ public class CityDetail extends Activity {
     private static String forecastDaysNum = "16";
     String lang = "en";
     ListView citylist;
+    Context mContext;
+    private Handler mhandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_detail);
         city = getIntent().getStringExtra("ForcastCityDetail");
+        mhandler=new Handler();
+     //   getFragmentManager().beginTransaction().add(R.id.container, new WeatherFragment()).commit();
+        GetDetailWeatherDetail(mContext, city);
 
-        getFragmentManager().beginTransaction().add(R.id.container, new WeatherFragment()).commit();
+    }
+
+    private void GetDetailWeatherDetail(final Context mContext, final String city) {
+
+
+        if (city.length() == 0) {
+            Toast.makeText(mContext, "Not able to find current location.Check ur connection", Toast.LENGTH_SHORT).show();
+
+        } else {
+            new Thread() {
+                public void run() {
+                    final JSONObject json = FetchForcastForDayJson.getJSON(mContext, city);
+                    if (json == null) {
+                        mhandler.post(new Runnable() {
+                            public void run() {
+
+                            }
+                        });
+                    } else {
+                        mhandler.post(new Runnable() {
+                            public void run() {
+                                RenderWeatherDay(json);
+                            }
+                        });
+                    }
+                }
+            }.start();
+        }
+
+
+    }
+
+    private void RenderWeatherDay(JSONObject json) {
+
+        try {
+            JSONObject jsondaycode=json.getJSONObject("cod");
+            String city_code=jsondaycode.getString("cod");
+            Log.i("City Code is", "City Code" + city_code);
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
     }
